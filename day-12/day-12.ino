38f30d477ff63e9540cd3184da7ccfec05e34c4d
Neil Burningham <kneilb@gmail.com>
2024-04-04 19:43:04 +0100

    day 12 & day12a

--- day-12/day-12.ino
+++ day-12/day-12.ino
@@ -8,18 +8,18 @@ const byte BUZZER_PIN = 10;
 const byte ROW_PINS[ROWS] = {5, 4, 3, 2};
 const byte COL_PINS[COLS] = {6, 7, 8, 9};
 
+// map buttons to numbers, so we can use them as an index into TONES
 const char BUTTONS[ROWS][COLS] = {
-    {'1', '2', '3', 'A'},
-    {'4', '5', '6', 'B'},
-    {'7', '8', '9', 'C'},
-    {'*', '0', '#', 'D'}};
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12},
+    {13, 14, 15, 16}};
 
-const unsigned int TONES[ROWS][COLS] = {
-    // a frequency tone for each button
-    {31, 93, 147, 208},
-    {247, 311, 370, 440},
-    {523, 587, 698, 880},
-    {1397, 2637, 3729, 0} // Use frequency of 0 for bottom right key to end tone.
+// a frequency tone for each button
+const unsigned int TONES[ROWS * COLS] = {
+    31, 93, 147, 208, 247, 311, 370, 440,
+    523, 587, 698, 880,
+    1397, 2637, 3729, 0 // Use frequency of 0 for bottom right key to end tone.
 };
 
 Keypad myAwesomePad = Keypad(makeKeymap(BUTTONS), ROW_PINS, COL_PINS, ROWS, COLS);
@@ -31,29 +31,17 @@ void setup()
 
 void loop()
 {
-    const char button_character = myAwesomePad.waitForKey();
+    const char button_number = myAwesomePad.waitForKey();
 
-    unsigned int tone_frequency = 0; // Frequency to use for tone (default to 0, no tone)
-    for (byte i = 0; i < ROWS; i++)
-    {
-        for (byte j = 0; j < COLS; j++)
-        {
-            if (button_character == BUTTONS[i][j])
-            { // found it, get the corresponding tone
-                tone_frequency = TONES[i][j];
-            }
-
-        } // end j loop
-    }     // end i loop
+    // Frequency to use for tone
+    unsigned int tone_frequency = TONES[button_number - 1];
 
     Serial.print("Key: "); //   send the button_character to serial monitor...
-    Serial.print(button_character);
+    Serial.print(button_number + '0');
     Serial.print("   Freq: ");
     Serial.println(tone_frequency);
 
-    /*
-     * The tone() function plays a tone until stopped.  The code continues to run as the tone plays.
-     */
+    // The tone() function plays a tone until stopped.  The code continues to run as the tone plays.
     if (tone_frequency > 0)
     {                                     // If tone frequency greater than 0...
         tone(BUZZER_PIN, tone_frequency); // ...then play the tone at that frequency until stopped
