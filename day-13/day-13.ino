01078343908e019338e907ca9dbec66b9fc7ad11
Neil Burningham <kneilb@gmail.com>
2024-04-05 10:31:13 +0100

    Day 13: progress!

--- day-13/day-13.ino
+++ day-13/day-13.ino
@@ -4,51 +4,76 @@
 const byte ROWS = 4;
 const byte COLS = 4;
 
-const byte BUZZER_PIN = 10;
+const byte PIN_LENGTH = 4;
+char current_pin[PIN_LENGTH] = { '0', '0', '0', '0'};
+
 const byte ROW_PINS[ROWS] = {5, 4, 3, 2};
 const byte COL_PINS[COLS] = {6, 7, 8, 9};
 
 // map buttons to numbers, so we can use them as an index into TONES
 const char BUTTONS[ROWS][COLS] = {
-    {1, 2, 3, 4},
-    {5, 6, 7, 8},
-    {9, 10, 11, 12},
-    {13, 14, 15, 16}};
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
 
-// a frequency tone for each button
-const unsigned int TONES[ROWS * COLS] = {
-    31, 93, 147, 208, 247, 311, 370, 440,
-    523, 587, 698, 880,
-    1397, 2637, 3729, 0 // Use frequency of 0 for bottom right key to end tone.
-};
+Keypad BadgerButtons = Keypad(makeKeymap(BUTTONS), ROW_PINS, COL_PINS, ROWS, COLS);
 
-Keypad myAwesomePad = Keypad(makeKeymap(BUTTONS), ROW_PINS, COL_PINS, ROWS, COLS);
+const byte BUZZER_PIN = 10;
 
 void setup()
 {
+    pinMode(BUZZER_PIN, OUTPUT);
+
     Serial.begin(9600);
+    delay(200);
+    Serial.println("Press the star thingy for a new pin or the noughts and crosses grid to log in. If you forgot then its just bad luck. Nothing we can do for ya.");
 }
 
 void loop()
 {
-    const char button_number = myAwesomePad.waitForKey();
+    char button_character = BadgerButtons.waitForKey();
 
     // Frequency to use for tone
-    unsigned int tone_frequency = TONES[button_number - 1];
+    tone(BUZZER_PIN, 880, 100);
 
-    Serial.print("Key: "); //   send the button_character to serial monitor...
-    Serial.print(button_number + '0');
-    Serial.print("   Freq: ");
-    Serial.println(tone_frequency);
-
-    // The tone() function plays a tone until stopped.  The code continues to run as the tone plays.
-    if (tone_frequency > 0)
-    {                                     // If tone frequency greater than 0...
-        tone(BUZZER_PIN, tone_frequency); // ...then play the tone at that frequency until stopped
-    }
-    else
+    if (button_character == '#')
     {
-        Serial.println("Stop tone");
-        noTone(BUZZER_PIN); // Stop pressed (tone frequency of 0) so stop any tone playing
+        bool access_allowed = validatePIN();
+        if (access_allowed)
+        {
+            Serial.println("Well done. You logged in. Even though this is the only reward. K bye.");
+        }
+        else
+        {
+            Serial.println("INTRUDER!!!!! or u forgot ur pin. probs more likely.");
+            Serial.println("Press the star thingy for a new pin or the noughts and crosses grid to log in.");
+        }
     }
+
+    if (button_character == '*')
+    {
+        bool access_allowed = validatePIN();
+
+        if (access_allowed)
+        {
+            Serial.println("Hi. Press some random numbers i guess.");
+
+            for (int i = 0; i < PIN_LENGTH; i++)
+            {
+                button_character = BadgerButtons.waitForKey();
+                tone(BUZZER_PIN, 880, 100);
+
+                current_pin[i] = button_character;
+                Serial.print("*");
+            }
+
+        }
+    }
+}
+
+bool validatePIN()
+{
+    //place holder (OF HELL)
+    return true;
 }
